00a3f8ffdc7b4a2c797d1d3ecedd75d069056d0e
Piotr Gankiewicz <spetz90@gmail.com>
2017-09-10 21:22:37 +0200

    Fixed base module

--- src/Collectively.Services.Supervisor/Modules/SupervisorModule.cs
+++ src/Collectively.Services.Supervisor/Modules/SupervisorModule.cs
@@ -6,7 +6,7 @@ namespace Collectively.Services.Supervisor.Modules
 {
     public class SupervisorModule : ModuleBase
     {
-        public SupervisorModule(ISupervisorService supervisorService)
+        public SupervisorModule(ISupervisorService supervisorService) : base (requireAuthentication: false)
         {
             Get("supervisor", args => Fetch<GetSupervisorResult, SupervisorResult>
                 (async x => await supervisorService.CheckServicesAsync()).HandleAsync());
